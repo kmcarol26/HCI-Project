f562369158ec4ccb5042826ab2cc6d9141d566c6
Monisha Carol Karise <kmcarol26@ccs.neu.edu>
2018-11-13 04:54:22 -0500

    Added index,toggle for different appointments to the student day view

--- public/project/views/student/controllers/private-appointment.controller.js
+++ public/project/views/student/controllers/private-appointment.controller.js
@@ -15,7 +15,7 @@
             var event = $rootScope.privateEvent;
             vm.initStartTime = new Date(event.startTime);
             vm.initEndTime = new Date(event.endTime);
-            vm.appointment = {startTime: vm.initStartTime, endTime : vm.initEndTime};
+            vm.appointment = {startTime: vm.initStartTime, endTime : vm.initEndTime, category : vm.category};
             vm.appointment.membercount = 1;
             $('[data-toggle="tooltip"]').tooltip();
         }
@@ -35,11 +35,11 @@
         function confirmAppointment(appointment) {
             var p;
             if(appointment.visibility == "Public") {
-                appointment.color = 'darkcyan';
+                appointment.color = 'dodgerblue';
                 p = true;
             }
             else {
-                appointment.color = 'darkblue';
+                appointment.color = 'tomato';
                 p = false;
             }
             $(".modal-backdrop").remove();
@@ -47,6 +47,8 @@
                 title: appointment.summary,
                 start: vm.appointment.startTime,
                 end: vm.appointment.endTime,
+                category:appointment.category,
+                summary:appointment.summary,
                 color:appointment.color,
                 editable: false,
                 public:p
